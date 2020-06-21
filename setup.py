210cf5d3aca868545b15fce393a78c2c35a78e92
Isaac To <isaacto@gmail.com>
2020-06-21 11:24:11 +0800

    Set version number to 0.0.1
    Signed-off-by: Isaac To <isaacto@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='cronrepo',
-    version='0.1',
+    version='0.0.1',
     author='Isaac To',
     author_email='isaac.to@gmail.com',
     description='Maintain a set of cron jobs in your code repository',
